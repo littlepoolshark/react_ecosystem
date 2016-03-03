14620b8ac7590a11b0deb1ec9292c1ec84556480
sam liu <1145694956@qq.com>
2016-03-03 19:18:54 +0800

    将进度条的默认值改为60

--- component/utilities/circleProcessBar/CircleProcessBar.jsx
+++ component/utilities/circleProcessBar/CircleProcessBar.jsx
@@ -4,7 +4,7 @@ var React=require("react");
 var CircleProcessBar=React.createClass({
     getDefaultProps:function(){
         return {
-            percentage:50
+            percentage:60
         }
     },
     propTypes:{
